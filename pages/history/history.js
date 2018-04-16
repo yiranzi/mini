d078ea879e53c52e7848d021f5af082f940e9437
yiranzi <570932296@qq.com>
2018-04-17 01:46:40 +0800

    upgrade: 提交

--- pages/history/history.js
+++ pages/history/history.js
@@ -16,6 +16,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.setNavigationBarColor({
+      frontColor:'#ffffff',
+      backgroundColor:'#ed5629'
+    })
     wx.setNavigationBarTitle({
       title: '历史数据'
     })
---
+++ pages/ucenter/ucenter.js
@@ -0,0 +1,92 @@
+// pages/ucenter/ucenter.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userInfo: {}
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.setNavigationBarColor({
+      frontColor:'#ffffff',
+      backgroundColor:'#ed5629'
+    })
+    wx.setNavigationBarTitle({
+      title: '个人中心'
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    if (getApp().globalData.userInfo) {
+      this.setData({
+        userInfo: getApp().globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      getApp().userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+        })
+      }
+    }
+  },
+
+  enterHistory: function () {
+    let url = `/pages/myhistory/myhistory`
+    wx.navigateTo({
+      url: url
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
