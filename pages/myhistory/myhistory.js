4808042e52c814e19bd0ae91c11bb19968b46d85
Andy <andy.wei@xiaozao.org>
2018-04-17 02:27:25 +0800

    样式提交

--- pages/myhistory/myhistory.js
+++ pages/myhistory/myhistory.js
@@ -5,7 +5,61 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    userInfo: {},
+    array: [1,2,3],
+    equipList: [
+      {
+        equipTitle: '智能床垫',
+        time: '',
+        equipAttrArr: [
+          {
+            attrName: '睡眠时间',
+            attrValue: '',
+            attrUnit: '暂无数据',
+            icon: 'https://jiuhetang-1256506523.cos.ap-chengdu.myqcloud.com/attr_sleep.png'
+          },
+          {
+            attrName: '心率',
+            attrValue: -1,
+            attrUnit: '次/分钟',
+            icon: 'https://jiuhetang-1256506523.cos.ap-chengdu.myqcloud.com/attr_heart.png'
+          },
+        ],
+        status: '',
+      },
+      {
+        equipTitle: '血糖仪',
+        time: '',
+        equipAttrArr: [
+          {
+            attrName: '血糖',
+            attrValue: -1,
+            attrUnit: '毫摩/升',
+            icon: 'https://jiuhetang-1256506523.cos.ap-chengdu.myqcloud.com/attr_sugar.png'
+          }
+        ],
+        status: 2,
+      },
+      {
+        equipTitle: '智能手表',
+        time: '',
+        equipAttrArr: [
+          {
+            attrName: '心率',
+            attrValue: -1,
+            attrUnit: '次/分钟',
+            icon: 'https://jiuhetang-1256506523.cos.ap-chengdu.myqcloud.com/attr_heart.png'
+          },
+          {
+            attrName: '步数',
+            attrValue: -1,
+            attrUnit: '次/分钟',
+            icon: 'https://jiuhetang-1256506523.cos.ap-chengdu.myqcloud.com/attr_step.png'
+          }
+        ],
+        status: 2,
+      },
+    ]
   },
 
   /**
